5b3b99ca5102b1cd8f5cfc132956273e08f96b16
MohamedTarek512 <moh-tarek91@hotmail.com>
2024-06-22 20:32:17 +0200

    Update full-test.sh

--- full-test.sh
+++ full-test.sh
@@ -2,6 +2,8 @@
 # This script can be copied into your base directory for use with
 # automated testing using assignment-autotest.  It automates the
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
+export FORCE_UNSAFE_CONFIGURE=1
+
 set -e
 
 cd `dirname $0`
