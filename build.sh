a76ad03edb9978d9cd407ae9b225c97e9c52fbb1
MohamedTarek512 <moh-tarek91@hotmail.com>
2024-06-22 14:18:47 +0200

    add caching

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 #Script to build buildroot configuration
 #Author: Siddhant Jajoo
 
+export PATH=$PATH:/home/mohamed/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu/bin
+export PATH=$PATH:/home/mohamed/.buildroot-ccache
 source shared.sh
 
 EXTERNAL_REL_BUILDROOT=../base_external
