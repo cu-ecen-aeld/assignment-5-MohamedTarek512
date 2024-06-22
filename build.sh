e0b05c85aa27db237aee7eb0c6d1336f3462bae5
MohamedTarek512 <moh-tarek91@hotmail.com>
2024-06-22 20:08:19 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #Author: Siddhant Jajoo
 
 export PATH=$PATH:/home/mohamed/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu/bin
-export PATH=$PATH:/home/mohamed/.buildroot-ccache
+#export PATH=$PATH:/home/mohamed/.buildroot-ccache
 source shared.sh
 export PATH=$PATH:/home/mohamed/tools/arm-gnu-toolchain-13.2.Rel1-x86_64-aarch64-none-linux-gnu/bin
 EXTERNAL_REL_BUILDROOT=../base_external
