946ef2b028d3857c464349b4ed81a2ef3f70e8de
MohamedTarek512 <moh-tarek91@hotmail.com>
2024-06-23 13:35:39 +0200

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -13,11 +13,16 @@ AESD_ASSIGNMENTS_LICENSE_FILES = LICENSE
 AESDSOCKET_INSTALL_STAGING = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    aarch64-none-linux-gnu-gcc -Wall -Werror -o $(@D)/server/aesdsocket $(@D)/server/aesdsocket.cendef
+    aarch64-none-linux-gnu-gcc -Wall -Werror -o $(@D)/server/aesdsocket $(@D)/server/aesdsocket.c
+endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
     $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+endef
+
+define AESD_ASSIGNMENTS_INSTALL_STAGING_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(STAGING_DIR)/usr/bin/aesdsocket
 endef
 
 $(eval $(generic-package))
