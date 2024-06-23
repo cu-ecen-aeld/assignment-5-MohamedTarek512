8eb0025f8a5bb6d5e8615b56058e2d0efd080d99
MohamedTarek512 <moh-tarek91@hotmail.com>
2024-06-23 09:53:11 +0200

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
+AESD_ASSIGNMENTS_VERSION = fb52eb9
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-MohamedTarek512
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 AESD_ASSIGNMENTS_LICENSE = GPL-2.0
 AESD_ASSIGNMENTS_LICENSE_FILES = LICENSE
-
-# Add local path
-AESD_ASSIGNMENTS_SITE = /home/mohamed/assignment-5-MohamedTarek512/base_external/package/aesd-assignments
-AESD_ASSIGNMENTS_SITE_METHOD = local
 AESDSOCKET_INSTALL_STAGING = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
@@ -22,4 +22,3 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
-
